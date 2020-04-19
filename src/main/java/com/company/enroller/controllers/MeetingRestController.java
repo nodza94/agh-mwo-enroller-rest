f98c4288a70412db9ee426a23313b0e2af0e1c92
Joanna Nodzynska <jnodzynsk@gmail.com>
2020-04-19 17:14:55 +0200

    Endpoint for meetings: Add new meetings

--- src/main/java/com/company/enroller/controllers/MeetingRestController.java
+++ src/main/java/com/company/enroller/controllers/MeetingRestController.java
@@ -5,10 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import com.company.enroller.model.Meeting;
+import com.company.enroller.model.Participant;
 import com.company.enroller.persistence.MeetingService;
 
 @RestController
@@ -33,6 +35,17 @@ public class MeetingRestController {
 			return new ResponseEntity<Meeting>(HttpStatus.NOT_FOUND);
 		}
 		return new ResponseEntity<Meeting>(meeting, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "", method = RequestMethod.POST) 
+	public ResponseEntity<?> registerMeeting(@RequestBody Meeting meeting){
+		Meeting foundParticipant = meetingService.findById(meeting.getId());
+		if (foundParticipant != null) {
+			return new ResponseEntity<String>(
+					"Unable to register. Meeting " + meeting.getId() + " already exists", HttpStatus.CONFLICT);
+		}
+		meetingService.add(meeting);
+		return new ResponseEntity<Meeting>(meeting, HttpStatus.CREATED);
 	}
 
 	/*
